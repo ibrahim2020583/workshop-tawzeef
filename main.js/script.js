c08c865bd88dea5287a8804594c42d9cdf48c940
ibrahem <ibrahim.elmarasy@yahoo.com>
2021-01-17 01:00:59 +0200

    add s-find jop& ui of s- FEATURED CANDIDATES some of header

--- main.js/script.js
+++ main.js/script.js
@@ -5,3 +5,11 @@ window.addEventListener('scroll' , ()=> {
     let nav = document.querySelector('.navbar')
     nav.classList.toggle('sticky' , window.scrollY > 200)
 })
+// slick slider
+$('.featured-candidates').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+  autoplaySpeed: 2000
+  });
