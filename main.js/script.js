07cfaa3721af18fe588d7ab8472bfbd3afd5e31c
ibrahem <ibrahim.elmarasy@yahoo.com>
2021-01-17 16:42:22 +0200

    3 section are finshed

--- main.js/script.js
+++ main.js/script.js
@@ -3,13 +3,45 @@ AOS.init();
 // stickynavbar
 window.addEventListener('scroll' , ()=> {
     let nav = document.querySelector('.navbar')
-    nav.classList.toggle('sticky' , window.scrollY > 200)
+    let btn = document.querySelector(".back-top")
+    nav.classList.toggle('sticky' , window.scrollY > 200),
+    btn.classList.toggle('d-block' , window.scrollY > 200)
 })
 // slick slider
 $('.featured-candidates').slick({
     infinite: true,
-    slidesToShow: 5,
+    slidesToShow: 3,
     slidesToScroll: 2,
     autoplay: true,
   autoplaySpeed: 2000
+  });
+//   campony slider
+$('.our-companies').slick({
+    // infinite: true,
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplay: true,
+  autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
