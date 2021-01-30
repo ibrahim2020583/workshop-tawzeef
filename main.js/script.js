630a68f883716b3484b2bf4f36f4dd1fb5db1fe8
User Name <maelshaer83@gmail.com>
2021-01-30 18:00:21 +0200

    edite my section and add active hover nav-link

--- main.js/script.js
+++ main.js/script.js
@@ -45,3 +45,24 @@ $('.our-companies').slick({
       }
     ]
   });
+
+// Loading Screen
+
+$(window).load(function () {
+    
+    "use strict";
+    
+    // Loading Elements
+    
+    $(".loading-page .logo-web").fadeOut(2000, function () {
+        
+        // Show The Scroll
+
+        $("body").css("overflow", "auto");
+        
+        $(this).parent().fadeOut(2000, function () {
+            
+            $(this).remove();
+        });
+    });
+});
