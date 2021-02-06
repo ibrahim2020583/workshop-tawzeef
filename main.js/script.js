203fd296193eff524a219ad2da798852dd34051b
ibrahem <ibrahim.elmarasy@yahoo.com>
2021-02-06 20:43:16 +0200

    ['add section dream-job & add random image & change background']

--- main.js/script.js
+++ main.js/script.js
@@ -106,3 +106,29 @@ $(window).on("load resize", function() {
     $dropdown.off("mouseenter mouseleave");
   }
 });
+// random image
+let img = document.querySelector('#image');
+let arr = ['images/blog/blog_4.png' ,'images/blog/blog_2.png' ,'images/blog/single_blog_5.png' ,'images/blog/single_blog_1.png' ,];
+setInterval(()=>{
+  let randomNumber =Math.floor(Math.random()*arr.length);
+  img.src = arr[randomNumber];
+},1000)
+let img1 = document.querySelector('#image1');
+let arr1 = ['images/blog/single_blog_3.png' ,'images/blog/single_blog_1.png' ,'images/blog/blog_3.png' ,'images/blog/blog_4.png' ,];
+setInterval(()=>{
+  let randomNumber =Math.floor(Math.random()*arr1.length);
+  img1.src = arr[randomNumber];
+},3000)
+let img2 = document.querySelector('#image2');
+let arr2 = ['images/blog/blog_1.png' ,'images/blog/blog_2.png' ,'images/blog/single_blog_4.png' ,'images/blog/single_blog_2.png' ,];
+setInterval(()=>{
+  let randomNumber =Math.floor(Math.random()*arr2.length);
+  img2.src = arr[randomNumber];
+},4000)
+let img3 = document.querySelector('#image3');
+let arr3 = ['images/blog/single_blog_3.png' ,'images/blog/single_blog_5.png' ,'images/blog/blog_3.png' ,'images/blog/blog_4.png' ,];
+setInterval(()=>{
+  let randomNumber =Math.floor(Math.random()*arr3.length);
+  img3.src = arr[randomNumber];
+},2000)
+// random image
